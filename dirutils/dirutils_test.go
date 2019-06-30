49d579a866cfd69e0b60d3b437967fa7fdabe6c9
marguerite <i@marguerite.su>
2019-06-30 12:37:15 +0800

    update fileutils.Remove

--- dirutils/dirutils_test.go
+++ dirutils/dirutils_test.go
@@ -5,19 +5,10 @@ import (
 	"testing"
 )
 
-func TestLs(t *testing.T) {
-	if dirs, e := Ls(".", "file"); !reflect.DeepEqual(dirs, []string{"dir.go", "dir_test.go"}) || e != nil {
-		t.Error("dir.Ls test failed.")
+func TestLsFile(t *testing.T) {
+	if files, e := Ls(".", "file"); !reflect.DeepEqual(files, []string{"dirutils.go", "dirutils_test.go"}) || e != nil {
+		t.Error("dirutils.Ls test failed.")
 	} else {
-		t.Log("dir.Ls test passed.")
-	}
-}
-
-func TestLsf(t *testing.T) {
-	if dirs, e := Ls(".", "file"); !reflect.DeepEqual(dirs, []string{"dir.go", "dir_test.go"}) ||
-		e != nil {
-		t.Error("dir.Lsf test failed.")
-	} else {
-		t.Log("dir.Lsf test passed.")
+		t.Log("dirutils.Ls test passed.")
 	}
 }
