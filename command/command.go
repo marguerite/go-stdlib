fcea73b8dc2ea6bc9ac8a5eacbd7f02359ff349c
marguerite <i@marguerite.su>
2019-07-17 12:27:24 +0800

    command: use exec.LookPath, avoid reinventing the wheel

--- command/command.go
+++ command/command.go
@@ -1,11 +1,9 @@
 package command
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -22,76 +20,38 @@ func Environ(env string) (string, error) {
 	return val, nil
 }
 
-func search(cmd string) (string, error) {
-	// the cmd definitely exists
-	f, _ := os.Stat(cmd)
+// Search if an executable exists
+func Search(cmd string) (string, error) {
+	f, err := os.Stat(cmd)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// look in $Path
+			f1, err1 := exec.LookPath(cmd)
+			if err1 != nil {
+				return "", fmt.Errorf("%s doesn't exist in current directory or $PATH.", cmd)
+			}
+			return f1, nil
+		} else {
+			return "", fmt.Errorf("Another unhandled non-IsNotExist PathError occurs %s", err.Error())
+		}
+	}
 	if f.IsDir() {
 		return "", fmt.Errorf("%s is a directory", cmd)
 	}
-	if strings.Contains(f.Mode().Perm().String(), "-rwxr-") {
+	if isExecutable(f) {
 		return cmd, nil
 	}
-	return "",f
-}
-
-// Search if an executable exists
-func Search(cmd string) (string, error) {
-  f, err := os.Stat(cmd)
-	if err != nil {
-		if os.IsNotExist(err) {
-      // add $PATH and try again
-		} else {
-			return "", fmt.Errof("Another unhandled non-IsNotExist PathError occurs %s", err.Error())
-		}
-	}
-
-	// whether to add path.
-	if filepath.Dir(cmd) == "." {
-		if _, err := os.Stat(cmd); os.IsNotExist(err) {
-			pathEnv, err := Environ("PATH")
-			if err != nil {
-				return "", errors.New("System $PATH not set or empty.")
-			}
-			ok := false
-			command := ""
-			for _, v := range strings.Split(pathEnv, ":") {
-				c := filepath.Join(v, cmd)
-				if _, err := os.Stat(c); !os.IsNotExist(err) {
-					command = c
-					ok = true
-					break
-				}
-			}
-			if ok {
-				cmd = command
-			} else {
-				return "", fmt.Errorf("Can not find executable %s", cmd)
-			}
-		}
-	} else {
-		if _, err := os.Stat(cmd); os.IsNotExist(err) {
-			return "", fmt.Errorf("Can not find executable %s", cmd)
-		}
-	}
-
-	if f, _ := os.Stat(cmd); strings.Contains(f.Mode().String(), "-rwxr-") {
-		return cmd, nil
-	}
-	return cmd, fmt.Errorf("%s exists but not executable.", cmd)
+	return "", fmt.Errorf("%s is a file but has no exec permission", cmd)
 }
 
 // Run run command with options, returns output, ExitStatus and error
 func Run(cmd string, opts ...string) (string, int, error) {
-	c, err := Search(cmd)
-	if err != nil {
-		return "", -1, err
-	}
-	out, err := exec.Command(c, opts...).Output()
+	out, err := exec.Command(cmd, opts...).Output()
 
-	fmt.Printf("Executing: %s %s\n", c, strings.Join(opts, " "))
+	fmt.Printf("Executing: %s %s\n", cmd, strings.Join(opts, " "))
 
 	if err != nil {
-		if msg, ok := err.(*exec.Error); ok {
+		if _, ok := err.(*exec.Error); ok {
 			return string(out), -1, err
 		}
 
