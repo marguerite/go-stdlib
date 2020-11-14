8c4bed7671612b3716952106bddce2b413f93131
marguerite <i@marguerite.su>
2020-11-14 13:56:01 +0800

    stringutils.Gsub

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -1,5 +1,15 @@
 package stringutils
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// Contains if s contains any of the substrings, all of the substrings,
+// and index of their occurances
 func Contains(s string, substrings ...string) (bool, bool, map[string]int) {
 	if len(s) == 0 {
 		return false, false, nil
@@ -67,4 +77,72 @@ func Contains(s string, substrings ...string) (bool, bool, map[string]int) {
 		return true, false, m1
 	}
 	return false, false, nil
+}
+
+// Gsub like ruby gsub, match can be string or *regexp.Regexp, replacer can be map (hash in ruby) or string
+// the string match can have "|", then the conditional sub strings can be replaced by the value specified in replacer map
+// and the replacer string can refer to matched target in match regexp via "\1" or "\2"
+func Gsub(orig string, match, replacer interface{}) string {
+	// prepare source
+	var source [][]string
+	if val, ok := match.(*regexp.Regexp); ok {
+		if !val.MatchString(orig) {
+			return orig
+		}
+		m := val.FindAllStringSubmatch(orig, -1)
+		source = m
+	}
+	if val, ok := match.(string); ok {
+		s := strings.Split(val, "|")
+		for _, v := range s {
+			source = append(source, []string{v})
+		}
+	}
+
+	// prepare replace
+	if val, ok := replacer.(string); ok {
+		var rplr []string
+		for i, v := range val {
+			if v == '\\' {
+				r := []rune{v}
+				for j := i + 1; j < len(val); j++ {
+					if !unicode.IsDigit([]rune(val)[j]) {
+						break
+					}
+					r = append(r, []rune(val)[j])
+				}
+				rplr = append(rplr, string(r))
+			}
+		}
+
+		// actual replace
+		for i, v := range source {
+			r := val
+			for _, v1 := range rplr {
+				idxStr := strings.TrimPrefix(v1, "\\")
+				idx, _ := strconv.Atoi(idxStr)
+				// idx+i, eg: in regexp `(m)arguerite|diagnos(e)`, m is \1 and e is \2, but in gsub they should be all \1
+				if idx+i > len(v)-1 {
+					err := fmt.Errorf("%d is greater than %d, over-capacity", idx+i, len(v)-1)
+					panic(err)
+				}
+				r = strings.Replace(r, v1, v[idx+i], 1)
+			}
+			orig = strings.Replace(orig, v[0], r, 1)
+		}
+
+		return orig
+	}
+
+	if val, ok := replacer.(map[string]string); ok {
+		for _, v := range source {
+			if val1, ok := val[v[0]]; ok {
+				orig = strings.Replace(orig, v[0], val1, 1)
+			} else {
+				orig = strings.Replace(orig, v[0], "", 1)
+			}
+		}
+	}
+
+	return orig
 }
--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -1,8 +1,11 @@
 package stringutils
 
 import (
+	"regexp"
 	"testing"
 )
+
+const uri = "https://github.com/marguerite/diagnose"
 
 func TestContainsFullMatch(t *testing.T) {
 	s := "NotoColorEmoji.ttf"
@@ -17,5 +20,32 @@ func TestContainsPartialMatch(t *testing.T) {
 	ok, full, matched := Contains(s, ".pfa", ".pfb", ".otf", ".ttf")
 	if !ok || full || len(matched) != 1 {
 		t.Error("[stringutils]: Contains partial match test failed")
+	}
+}
+
+func TextGsubRegexpMatchWithNormalTextReplacer(t *testing.T) {
+	re := regexp.MustCompile(`(m)arguerite|diagnos(e)`)
+	if Gsub(uri, re, "opensuse") != "https://github.com/opensuse/opensuse" {
+		t.Error("[stringutils]: Gsub regexp match with normal text replacer failed")
+	}
+}
+
+func TextGsubRegexpMatchWithMatchedTargetReferenceReplacer(t *testing.T) {
+	re := regexp.MustCompile(`(m)arguerite|diagnos(e)`)
+	if Gsub(uri, re, "(\\1)") != "https://github.com/(m)/(e)" {
+		t.Error("[stringutils]: Gsub regexp match with matched target reference replacer failed")
+	}
+}
+
+func TextGsubStringMatchWithNormalTextReplacer(t *testing.T) {
+	if Gsub(uri, "marguerite", "opensuse") != "https://github.com/opensuse/diagnose" {
+		t.Error("[stringutils]: Gsub string match with normal text replacer failed")
+	}
+}
+
+func TextGsubStringMatchWithMapReplacer(t *testing.T) {
+	m := map[string]string{"marguerite": "opensuse", "diagnose": "diagnostic"}
+	if Gsub(uri, "marguerite|diagnose", m) != "https://github.com/opensuse/diagnostic" {
+		t.Error("[stringutils]: Gsub string match with map replacer failed")
 	}
 }
