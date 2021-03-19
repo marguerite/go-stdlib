4bbf107a48d72158ecbc72467a3634001e73af9b
marguerite <i@marguerite.su>
2021-03-19 20:48:07 +0800

    1. refactor os-release with init();2. Version() should be float64

--- os-release/os-release.go
+++ os-release/os-release.go
@@ -8,29 +8,28 @@ import (
 	"strings"
 )
 
-// OsRelease map refer to /etc/os-release
-type OsRelease map[string]string
+var release map[string]string
 
-func (release *OsRelease) init() {
-	f, _ := ioutil.ReadFile("/etc/os-release")
+func init() {
+	f, err := ioutil.ReadFile("/etc/os-release")
+	if err != nil {
+		panic("no /etc/os-release")
+	}
+
 	scanner := bufio.NewScanner(bytes.NewReader(f))
 	for scanner.Scan() {
 		arr := strings.Split(scanner.Text(), "=")
-		(*release)[strings.ToLower(arr[0])] = strings.Replace(arr[1], "\"", "", -1)
+		release[strings.ToLower(arr[0])] = strings.Replace(arr[1], "\"", "", -1)
 	}
 }
 
 // Name The name of your Linux OS
 func Name() string {
-	release := OsRelease{}
-	release.init()
 	return release["pretty_name"]
 }
 
 // Version The version of your Linux OS
-func Version() int {
-	release := OsRelease{}
-	release.init()
-	version, _ := strconv.Atoi(release["version_id"])
+func Version() float64 {
+	version, _ := strconv.ParseFloat(release["version_id"], 64)
 	return version
 }
